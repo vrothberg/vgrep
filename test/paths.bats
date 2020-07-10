vrothberg/vgrep

commit 3738a019c0ecf300a77144ffd086a912aab91eed

grep commands: Do not pass '.' as an argument to grep/ripgrep
When building the grep or ripgrep commands to run, a dot ('.') is
appended to the list of arguments, to indicate that the current working
directory should be searched recursively.

Both grep (with its '-r' option) and ripgrep already search the current
working directory recursively if no other path is provided. Passing the
dot:

- Is useless if no other path (file or directory) to search is provided.
- Adds the content of the whole directory to the list of the files to
  search, if files or directories were provided on the command line.

This is inconvenient for searching a subdirectory, for example. Typing
'vgrep my_pattern foo/' will search for the pattern in files under foo/
but also in all other files under the current directory, which may take
longer and return undesired results.

This is also inconsistent with the behaviour for the 'git grep' command,
where only the arguments are passed and no '.' is appended.

Fix this by removing the dot appended to the command line. If desired,
the former behaviour can be obtained by passing the working directory on
the vgrep command line: 'vgrep my_pattern foo/ .'.

vgrep.go
@@ -232,7 +232,6 @@ func (v *vgrep) grep(args []string) {
 			"--color=always", "--no-heading", "--line-number",
 		}
 		cmd = append(cmd, args...)
-		cmd = append(cmd, ".")
 		greptype = RIPGrep
 	} else if usegit {
 		env = "HOME="
@@ -244,9 +243,8 @@ func (v *vgrep) grep(args []string) {
 		greptype = GITGrep
 	} else {
 		env = "GREP_COLORS='ms=01;31:mc=:sl=:cx=:fn=:ln=:se=:bn='"
-		cmd = []string{"grep", "-ZIn", "--color=always"}
+		cmd = []string{"grep", "-ZInr", "--color=always"}
 		cmd = append(cmd, args...)
-		cmd = append(cmd, "-r", ".")
 		greptype = v.getGrepType()
 	}
 	output, err := v.runCommand(cmd, env)

commit 87e6001804171c5f869f5bb5f107f17bf3e3c342

grep output: force printing filename for grep/ripgrep
In the case where a single file to search is passed to ripgrep (for
example: 'vgrep my_pattern foo.c'), ripgrep does not print the name of
the file in its output. Then vgrep tries to split the result string on
the NUL byte and to access the second substring, but panics because it
does not exist:

    panic: runtime error: index out of range [1] with length 1

This case never happened until recently because we would happen the
current working directory ('.') to the list of files to search, but this
was changed in a recent commit.

Fix by forcing grep and ripgrep to print the name of the file, even if
there is just one file to search.

Add a series of test to make sure we can grep with the different tools
by passing a directory or a file name.

Fixes: #103
Fixes: 3738a019c0ec ("grep commands: Do not pass '.' as an argument to grep/ripgrep")

vgrep.go
@@ -230,6 +230,7 @@ func (v *vgrep) grep(args []string) {
 		cmd = []string{
 			"rg", "-0", "--colors=path:none", "--colors=line:none",
 			"--color=always", "--no-heading", "--line-number",
+			"--with-filename",
 		}
 		cmd = append(cmd, args...)
 		greptype = RIPGrep
@@ -243,7 +244,7 @@ func (v *vgrep) grep(args []string) {
 		greptype = GITGrep
 	} else {
 		env = "GREP_COLORS='ms=01;31:mc=:sl=:cx=:fn=:ln=:se=:bn='"
-		cmd = []string{"grep", "-ZInr", "--color=always"}
+		cmd = []string{"grep", "-ZHInr", "--color=always"}
 		cmd = append(cmd, args...)
 		greptype = v.getGrepType()
 	}

test/paths.bats
@@ -1,0 +1,75 @@
+#!/usr/bin/env bats -t
+
+# Test that we support passing directories or files to search to the grep
+# tools. Check that we do not panic, that we find the good match, and that only
+# relevant paths are searched.
+
+# Passing one subdirectory to search
+
+SUBDIR=test/search_files
+
+@test "Search one subdir" {
+	run ./build/vgrep foo $SUBDIR
+	[ "$status" -eq 0 ]
+	[[ ${lines[1]} =~ "bar baz" ]]
+	# Check it does not match _this_ file
+	[[ ! ${lines[@]} =~ "SUBDIR" ]]
+}
+
+@test "Search one subdir and --no-git" {
+	run ./build/vgrep --no-git foo $SUBDIR
+	[ "$status" -eq 0 ]
+	[[ ${lines[1]} =~ "bar baz" ]]
+	[[ ! ${lines[@]} =~ "SUBDIR" ]]
+}
+
+@test "Search one subdir and --no-ripgrep" {
+	run ./build/vgrep --no-ripgrep foo $SUBDIR
+	[ "$status" -eq 0 ]
+	[[ ${lines[1]} =~ "bar baz" ]]
+	[[ ! ${lines[@]} =~ "SUBDIR" ]]
+}
+
+@test "Search one subdir and --no-git --no-ripgrep" {
+	run ./build/vgrep --no-git --no-ripgrep foo $SUBDIR
+	[ "$status" -eq 0 ]
+	[[ ${lines[1]} =~ "bar baz" ]]
+	[[ ! ${lines[@]} =~ "SUBDIR" ]]
+}
+
+# Passing a single file to search
+#
+# There is a difference with searching a directory, because some tools
+# (grep/ripgrep) do not print the filename by default in that case. This would
+# break output parsing if vgrep did not force them to print the filename
+# unconditionally.
+
+FILE=$SUBDIR/foobar.txt
+
+@test "Search single file" {
+	run ./build/vgrep foo $FILE
+	[ "$status" -eq 0 ]
+	[[ ${lines[1]} =~ "bar baz" ]]
+	[[ ! ${lines[@]} =~ "SUBDIR" ]]
+}
+
+@test "Search single file and --no-git" {
+	run ./build/vgrep --no-git foo $FILE
+	[ "$status" -eq 0 ]
+	[[ ${lines[1]} =~ "bar baz" ]]
+	[[ ! ${lines[@]} =~ "SUBDIR" ]]
+}
+
+@test "Search single file and --no-ripgrep" {
+	run ./build/vgrep --no-ripgrep foo $FILE
+	[ "$status" -eq 0 ]
+	[[ ${lines[1]} =~ "bar baz" ]]
+	[[ ! ${lines[@]} =~ "SUBDIR" ]]
+}
+
+@test "Search single file and --no-git --no-ripgrep" {
+	run ./build/vgrep --no-git --no-ripgrep foo $FILE
+	[ "$status" -eq 0 ]
+	[[ ${lines[1]} =~ "bar baz" ]]
+	[[ ! ${lines[@]} =~ "SUBDIR" ]]
+}