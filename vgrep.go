vrothberg/vgrep

commit 88894b1c7b7e8f40131a5127dd6e87109475a82d

interactive: switch from bufio Scanner to (readline-like) liner
Use the readline Go library to process commands in interactive mode. It
provides better support for input (such as directional arrows) and a
number of classic readline shortcuts [0]. It also does command history,
which can be handy for editing the range of a previous command for
example. Autocompletion also exists but is not implemented in this
commit (the one-letter commands hardly need any completion).

Note that we loose bold marking on the prompt (or the ability to use
other ANSI control codes for the prompt in the future), because the
library explicitly disable it to avoid compatibility issues with Windows
[1].

[0] https://github.com/peterh/liner#line-editing
[1] https://github.com/peterh/liner/commit/160e3795572865ddd53357b3c3744fef57925885

vgrep.go
@@ -24,6 +24,7 @@ import (
 
 	"github.com/jessevdk/go-flags"
 	"github.com/nightlyone/lockfile"
+	"github.com/peterh/liner"
 	"github.com/sirupsen/logrus"
 	"github.com/vrothberg/vgrep/internal/ansi"
 	"github.com/vrothberg/vgrep/internal/colwriter"
@@ -497,18 +498,18 @@ func sortKeys(m map[string]int) []string {
 // user input matches a vgrep selector commandShow will be executed. It will
 // prompt the user for commands if we're running in interactive mode.
 func (v *vgrep) commandParse() {
+	line := liner.NewLiner()
+	defer line.Close()
+	line.SetCtrlCAborts(true)
+
 	nextInput := func() string {
-		scanner := bufio.NewScanner(os.Stdin)
-		fmt.Print(ansi.Bold("Enter a vgrep command: "))
-		if !scanner.Scan() {
+		usrInp, err := line.Prompt("Enter a vgrep command: ")
+		if err != nil {
 			// Either we hit an error or EOF (ctrl+d)
-			if err := scanner.Err(); err != nil {
-				fmt.Fprintf(os.Stderr, "error parsing user input: %v", err)
-			}
-			fmt.Println()
+			fmt.Fprintf(os.Stderr, "error parsing user input: %v\n", err)
 			os.Exit(1)
 		}
-		usrInp := scanner.Text()
+		line.AppendHistory(usrInp)
 		logrus.Debugf("user input: %q", usrInp)
 		return usrInp
 	}

go.mod
@@ -4,6 +4,7 @@ require (
 	github.com/jessevdk/go-flags v1.4.0
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/nightlyone/lockfile v1.0.0
+	github.com/peterh/liner v1.2.0
 	github.com/sirupsen/logrus v1.6.0
 	github.com/stretchr/testify v1.4.0 // indirect
 	golang.org/x/sys v0.0.0-20200509044756-6aff5f38e54f // indirect

go.sum
@@ -11,8 +11,12 @@ github.com/kr/pty v1.1.1 h1:VkoXIwSboBpnk99O/KFauAEILuNHv5DVFKZMBN/gUgw=
 github.com/kr/pty v1.1.1/go.mod h1:pFQYn66WHrOpPYNljwOMqo10TkYh1fy3cYio2l3bCsQ=
 github.com/kr/text v0.1.0 h1:45sCR5RtlFHMR4UwH9sdQ5TC8v0qDQCHnXt+kaKSTVE=
 github.com/kr/text v0.1.0/go.mod h1:4Jbv+DJW3UT/LiOwJeYQe1efqtUx/iVham/4vfdArNI=
+github.com/mattn/go-runewidth v0.0.3 h1:a+kO+98RDGEfo6asOGMmpodZq4FNtnGP54yps8BzLR4=
+github.com/mattn/go-runewidth v0.0.3/go.mod h1:LwmH8dsx7+W8Uxz3IHJYH5QSwggIsqBzpuz5H//U1FU=
 github.com/nightlyone/lockfile v1.0.0 h1:RHep2cFKK4PonZJDdEl4GmkabuhbsRMgk/k3uAmxBiA=
 github.com/nightlyone/lockfile v1.0.0/go.mod h1:rywoIealpdNse2r832aiD9jRk8ErCatROs6LzC841CI=
+github.com/peterh/liner v1.2.0 h1:w/UPXyl5GfahFxcTOz2j9wCIHNI+pUPr2laqpojKNCg=
+github.com/peterh/liner v1.2.0/go.mod h1:CRroGNssyjTd/qIG2FyxByd2S8JEAZXBl4qUrZf8GS0=
 github.com/pmezard/go-difflib v1.0.0 h1:4DBwDE0NGyQoBHbLQYPwSUPoCMWR5BEzIk/f1lZbAQM=
 github.com/pmezard/go-difflib v1.0.0/go.mod h1:iKH77koFhYxTK1pcRnkKkqfTogsbg7gZNVY4sRDYZ/4=
 github.com/sirupsen/logrus v1.6.0 h1:UBcNElsrwanuuMsnGSlYmtmgbb23qDR5dG+6X6Oo89I=

test/interactive.bats
@@ -6,8 +6,7 @@
 p
 q
 EOF
-	# Current shell returns 1 in this case, skip status check
-	#[ "$status" -eq 0 ]
+	[ "$status" -eq 0 ]
 	# We expect 3 results, but there is also a prompt line in the output
 	[[ ${#lines[*]} -eq 4 ]]
 	[[ ${lines[0]} =~ "zero" ]]

vendor/github.com/mattn/go-runewidth/.travis.yml
@@ -1,0 +1,8 @@
+language: go
+go:
+  - tip
+before_install:
+  - go get github.com/mattn/goveralls
+  - go get golang.org/x/tools/cmd/cover
+script:
+    - $HOME/gopath/bin/goveralls -repotoken lAKAWPzcGsD3A8yBX3BGGtRUdJ6CaGERL

vendor/github.com/mattn/go-runewidth/LICENSE
@@ -1,0 +1,21 @@
+The MIT License (MIT)
+
+Copyright (c) 2016 Yasuhiro Matsumoto
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.

vendor/github.com/mattn/go-runewidth/README.mkd
@@ -1,0 +1,27 @@
+go-runewidth
+============
+
+[![Build Status](https://travis-ci.org/mattn/go-runewidth.png?branch=master)](https://travis-ci.org/mattn/go-runewidth)
+[![Coverage Status](https://coveralls.io/repos/mattn/go-runewidth/badge.png?branch=HEAD)](https://coveralls.io/r/mattn/go-runewidth?branch=HEAD)
+[![GoDoc](https://godoc.org/github.com/mattn/go-runewidth?status.svg)](http://godoc.org/github.com/mattn/go-runewidth)
+[![Go Report Card](https://goreportcard.com/badge/github.com/mattn/go-runewidth)](https://goreportcard.com/report/github.com/mattn/go-runewidth)
+
+Provides functions to get fixed width of the character or string.
+
+Usage
+-----
+
+```go
+runewidth.StringWidth("つのだ☆HIRO") == 12
+```
+
+
+Author
+------
+
+Yasuhiro Matsumoto
+
+License
+-------
+
+under the MIT License: http://mattn.mit-license.org/2013

commit af6c384b478e95f7a3d7411642df02b58e7d46d0

interactive: close command-line editor on early os.Exit()
If we fail to get input from the prompt, typically because the user
types Ctrl-C or sends EOF (e.g. Ctrl-D), then the defer to close the
readline-like command line editor is not executed, leaving the terminal
in a Weird State. I'm not sure what is changed exactly, but later
calling other command-line prompts (e.g. "git add -p", typing a SSH
password) will have the "Enter" key not behaving correctly.

Fix by closing the object before exiting. Looking at the code in
vgrep.go, it seems that all other calls to os.Exit() are in the main
function (i.e. not between liner.NewLiner() and return from
commandParse()), so we should not hit this issue elsewhere. It may still
happen if vgrep panics (but then that would likely be another bug in
itself). In such a case, note that typing "reset" in the terminal resets
the state and also fix the issue.

Fixes: #106
Fixes: 88894b1c7b7e ("interactive: switch from bufio Scanner to (readline-like) liner")

vgrep.go
@@ -506,6 +506,7 @@ func (v *vgrep) commandParse() {
 		usrInp, err := line.Prompt("Enter a vgrep command: ")
 		if err != nil {
 			// Either we hit an error or EOF (ctrl+d)
+			line.Close()
 			fmt.Fprintf(os.Stderr, "error parsing user input: %v\n", err)
 			os.Exit(1)
 		}