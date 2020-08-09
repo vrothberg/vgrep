f5165ca39319c8511a122990f82dd3afbb37d7c4
Valentin Rothberg <rothberg@redhat.com>
2020-08-09 17:24:42 +0200

    coverage tests
    Enable coverage tests in the CI.
    
    Note that we need enforce using the upstream version of bats as the
    Ubuntu one (used in golang:1.14) is buggy.
    
    Signed-off-by: Valentin Rothberg <rothberg@redhat.com>

---
+++ vgrep_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMain(_ *testing.T) {
+	// Do nothing. We just need dummy to make `ginkgo` happy. Without that,
+	// `ginkgo` would try to execute the _coverage_test.go _despite_ the
+}
