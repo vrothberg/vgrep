vrothberg/vgrep

commit a65a7b50b74e490d60be292286a42b71a8fa05bb

log parsing errors
If the CLI parses fails make sure to log the error.

Signed-off-by: Valentin Rothberg <rothberg@redhat.com>

vgrep.go
@@ -92,6 +92,7 @@ func main() {
 	parser := flags.NewParser(&v, flags.Default|flags.IgnoreUnknown)
 	args, err := parser.ParseArgs(os.Args[1:])
 	if err != nil {
+		logrus.Errorf("%v", err)
 		os.Exit(1)
 	}
 

commit 8bed785ca26c4b69b49b6bfe07a91016ddb559a4

don't print help message twice
Fix a regression introduced by commit a65a7b50b74e where the help
message is printed twice; let's rely on the parser to print errors.

Fixes: #159
Signed-off-by: Valentin Rothberg <rothberg@redhat.com>

vgrep.go
@@ -95,7 +95,8 @@ func main() {
 	parser := flags.NewParser(&v, flags.Default|flags.IgnoreUnknown)
 	args, err := parser.ParseArgs(os.Args[1:])
 	if err != nil {
-		logrus.Errorf("%v", err)
+		// Don't print the error to make sure the help message is printed once.
+		// In other words, let's rely on the parser to print errors.
 		os.Exit(1)
 	}
 